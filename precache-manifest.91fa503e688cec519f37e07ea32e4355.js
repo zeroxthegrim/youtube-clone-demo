0a774e0c5261c08f6ea18332718473ea09bd480f
Niksa <nhribljan@gmail.com>
2020-05-28 17:55:50 +0200

    Updates

---
+++ precache-manifest.91fa503e688cec519f37e07ea32e4355.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c178b9f91d9e8379dda2b6fa40e00863",
+    "url": "/youtube-clone-demo/index.html"
+  },
+  {
+    "revision": "c01856b03fd2158bea5b",
+    "url": "/youtube-clone-demo/static/js/2.1fa11446.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/youtube-clone-demo/static/js/2.1fa11446.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc4a7dbf80c69864aa7c",
+    "url": "/youtube-clone-demo/static/js/main.bec6b165.chunk.js"
+  },
+  {
+    "revision": "8a66bd85001d25db0466",
+    "url": "/youtube-clone-demo/static/js/runtime-main.0b22bdcf.js"
+  }
+]);
